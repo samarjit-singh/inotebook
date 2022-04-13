e3b385994a79e8a027234c259b7589edd93f6b0d
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-04-13 17:20:51 +0530

    storing data in database using mongoose

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  obj = {
-    a: "thios",
-    number: 34,
-  };
-  res.json(obj);
+// Cretae a User using: POST "/api/auth/" . dosen't require auth
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 });
 
 module.exports = router;
