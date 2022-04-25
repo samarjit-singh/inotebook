1857b9bf73aab950ec97edb0cb72ad02771ac4d5
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-04-26 00:52:56 +0530

    react context api

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <NoteState>
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </NoteState>
   );
 }
 
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,28 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+
+const NoteState = (props) => {
+  const s1 = {
+    name: "samar",
+    class: "5b",
+  };
+
+  const [state, setState] = useState(s1);
+
+  const update = () => {
+    setTimeout(() => {
+      setState({
+        name: "samarjit",
+        class: "10b",
+      });
+    }, 1000);
+  };
+
+  return (
+    <NoteContext.Provider value={{ state, update }}>
+      {props.children}
+    </NoteContext.Provider>
+  );
+};
+
+export default NoteState;
