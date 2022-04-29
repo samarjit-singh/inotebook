bacc5e2d70d7f12998452885daf1843c79920209
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-04-29 16:14:13 +0530

    added noteitem in a seperate note componen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,6 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import Notes from "./Notes";
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const { notes, setNotes } = context;
   return (
     <div>
       <div className="container my-3">
@@ -49,12 +46,7 @@ const Home = () => {
         </form>
       </div>
 
-      <div className="container my-3">
-        <h2>Your Note</h2>
-        {notes.map((note) => {
-          return note.title;
-        })}
-      </div>
+      <Notes />
     </div>
   );
 };
