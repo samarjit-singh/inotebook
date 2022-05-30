ae83b37c5ab7da382519110b48980c7ff1c38f6d
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-05-31 00:14:16 +0530

    added fonts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
+import { Alert } from "./components/Alert";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div>
         <BrowserRouter>
           <Navbar />
+          <Alert message="hello world"/>
           <div className="container">
             <Routes>
               <Route path="/home" element={<Home />} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <h2>Add a note</h2>
         <form className="my-3">
           <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
+            <label htmlfor="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
@@ -21,7 +21,7 @@ const Home = () => {
             </div>
           </div>
           <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
+            <label htmlfor="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
@@ -36,7 +36,7 @@ const Home = () => {
               className="form-check-input"
               id="exampleCheck1"
             />
-            <label className="form-check-label" for="exampleCheck1">
+            <label className="form-check-label" htmlfor="exampleCheck1">
               Check me out
             </label>
           </div>
