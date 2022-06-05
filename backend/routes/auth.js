959440c849195655ba6909688fad7c0b205749d2
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-06-05 22:27:30 +0530

    added signup component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -21,10 +21,11 @@ router.post(
   ],
 
   async (req, res) => {
+    let success = false;
     // If there are errors, return bad request and the error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
     // check whether the user with this email exist already
     try {
@@ -32,7 +33,7 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ error: "Sorry a user with this email already exist" });
+          .json({success, error: "Sorry a user with this email already exist" });
       }
 
       // salting and hashing our password using bcrypt library
@@ -55,7 +56,8 @@ router.post(
       const authtoken = jwt.sign(data, JWT_SECRET);
 
       // res.json(user);
-      res.send({ authtoken });
+      success = true;
+      res.json({ success,authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error");
