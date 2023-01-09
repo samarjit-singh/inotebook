0a221ae1f600b08b4ea2b9e2ebccbcc20eab2b4b
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2023-01-10 02:58:01 +0530

    connected to mongo compass

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,13 @@
 const mongoose = require("mongoose");
 const mongoURI = "mongodb://localhost:27017/inotebook";
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 // function to connect to mongo
 const connectToMongo = () => {
-  mongoose.connect(mongoURI, () => {
+  mongoose.connect(process.env.MONGO_URL, () => {
     console.log("Connected to Mongo Successfully");
   });
 };
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const connectToMongo = require("./db");
 const express = require("express");
 var cors = require("cors");
 
+
+
 connectToMongo();
 const app = express();
 const port = 5000;
@@ -13,6 +15,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`iNotebook backend listening on port http://localhost:${port}`);
 });
